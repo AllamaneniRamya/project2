2100662f923839a1485a36d3998e7c1afc0a9c7b
ramya <a.ramya267@gmail.com>
2024-01-04 14:10:09 +0530

    Add .gitignore file

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RainbowSchoolsStudentData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = "studentdata.txt";
+            try
+            {
+                string[] studentData = File.ReadAllLines("C:\\Users\\Administrator\\OneDrive\\Desktop\\Project2\\studentdata.txt");
+                foreach (var student in studentData)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"File not found: {filepath}");
+            }
+        }
+    }
+}
